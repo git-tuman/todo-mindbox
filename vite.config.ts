76b4b975f989c0fe6f5b326630ecd414b9c88a4c
Kirill <kirillvershov5500@gmail.com>
2025-08-29 16:29:21 +0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, ViteUserConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 const config: ViteUserConfig = defineConfig({
+  base: "/todo-mindbox/",
   plugins: [react()],
   test: {
     globals: true,

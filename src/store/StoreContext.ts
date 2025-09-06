0bda0b79a982288240d7e8c0bf1c5baf0cb1859c
Kirill <kirillvershov5500@gmail.com>
2025-09-06 22:21:56 +0300

    removed old state management

--- src/store/StoreContext.ts
+++
@@ -1,7 +0,0 @@
-// import { createContext, Dispatch } from "react";
-// import { Action, TodosState } from "../shared/types";
-
-// export const StoreContext = createContext<TodosState | undefined>(undefined);
-// export const StoreDispatch = createContext<Dispatch<Action> | undefined>(
-//   undefined
-// );
